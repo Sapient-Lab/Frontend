1068307c4aad9ad32051a30db79e63e3cd7ca595
AlexVillaTor <alexvillarroeltorrico@gmail.com>
2026-03-22 21:23:24 -0400

    restauracion

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -8,7 +8,7 @@ const navItems: NavItem[] = [
   { label: 'Equipo', path: '/app/equipo' },
   { label: 'Escaner de Protocolos', path: '/app/protocolos' },
   { label: 'Resultados', path: '/app/resultados' },
-  { label: 'Reportes IA', path: '/app/reportes' },
+  // { label: 'Reportes IA', path: '/app/reportes' },
   { label: 'Seguridad', path: '/app/seguridad' },
   { label: 'Tareas', path: '/app/tareas' },
   { label: 'Docs', path: '/app/docs' },
